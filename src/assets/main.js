840af48a8c3223328a7bf55fb0d9c61742504b9c
BaNburger <b.burger@gmx.eu>
2017-04-16 15:21:30 +0200

    step 9

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,13 +4,21 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (answer === "" OR attempt === "") {
+      setHiddenFields();
+    }
 }
 
 //implement new functions here
 
 function setHiddenFields() {
+  let attempt = 0;
   answer.value = Math.floor(Math.random()*10000).toString();
   while (answer.length < 4) {
     answer.value = "0" + answer.value;
   }
+}
+
+function setMessage(label) {
+  let message.innerHTML = label;
 }
